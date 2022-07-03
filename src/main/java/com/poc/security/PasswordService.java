b85ffa37714a2d4d098d9a9dcdf736088d816763
Anil-kumar540 <anilkumargannevaram@gmail.com>
2022-07-03 11:48:25 +0530

    first commit

---
+++ src/main/java/com/poc/security/PasswordService.java
@@ -0,0 +1,28 @@
+package com.poc.security;
+
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+
+public class PasswordService {
+	
+	public static String encode(String Txt) {
+		
+		Encoder encoder = Base64.getEncoder();
+		return  encoder.encodeToString(Txt.getBytes());
+	}
+	
+	public static String decode(String encodedTxt) {
+		Decoder decoder = Base64.getDecoder();
+		byte[] decoded_b_Array = decoder.decode(encodedTxt);
+		return new String(decoded_b_Array);
+	}
+	
+	public static void main(String[] args) {
+		String encodedTxt = PasswordService.encode("Anil");
+		System.out.println(encodedTxt);
+		String decodedTxt = PasswordService.decode(encodedTxt);
+		System.out.println(decodedTxt);
+	}
+
+}
