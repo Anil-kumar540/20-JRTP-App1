5a8e7572b1a234a18758365366e8226bf2857795
Anil-kumar540 <anilkumargannevaram@gmail.com>
2022-07-04 17:56:57 +0530

    unstashed 111 and fixed 111 after 121 fix

--- src/main/java/com/poc/security/PasswordService.java
+++ src/main/java/com/poc/security/PasswordService.java
@@ -7,14 +7,16 @@ import java.util.Base64.Encoder;
 public class PasswordService {
 	
 	public static String encode(String Txt) {
-		
+		//fixing bug 111
+		//int i = 10;
+		//int j = 20;
 		Encoder encoder = Base64.getEncoder();
 		return  encoder.encodeToString(Txt.getBytes());
 	}
 	
 	public static String decode(String encodedTxt) {
 		//fixing major bug 121,after stash partial-bugfix 111
-		String bug="fixed121";
+		//String bug="fixed121";
 		Decoder decoder = Base64.getDecoder();
 		byte[] decoded_b_Array = decoder.decode(encodedTxt);
 		return new String(decoded_b_Array);
